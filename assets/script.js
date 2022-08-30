1f039a8f702a2112ec188b604c8dd8d68cd46d45
Vegascruz <cruzv@students.westerntc.edu>
2022-08-30 17:47:23 -0500

    Added an assets folder, script file and added a fetch for the anime quotes page

---
+++ assets/script.js
@@ -0,0 +1,20 @@
+//NEED TO DO: link external js files
+
+fetch("https://api.waifu.pics/sfw/bonk")
+    .then(response => {
+        console.log(response);
+        return response.blob();
+    })
+    .then(blob =>{
+        console.log(blob);
+        document.getElementById('waifu-gif').src = URL.createObjectURL(blob);
+    })
+    .catch(error => {
+        
+        console.log('Error!');
+        console.log(error);
+    });
+
+    fetch('https://animechan.vercel.app/api/random')
+    .then(response => response.json())
+    .then(quote => console.log(quote))
