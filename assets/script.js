1aada84b323f0d4889ca050ee67f11c8f7a00950
Vegascruz <cruzv@students.westerntc.edu>
2022-08-30 20:13:20 -0500

    Got the waifu gif to pull up! Checkout the script page

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,7 @@
 //NEED TO DO: link external js files
 
-const animeChar = "";
-fetch("https://api.waifu.pics/sfw/bonk")
+
+/*fetch("https://api.waifu.pics/sfw/bonk")
     .then(response => {
         console.log(response);
         return response.blob();
@@ -15,14 +15,23 @@ fetch("https://api.waifu.pics/sfw/bonk")
         console.log('Error!');
         console.log(error);
     });
+    */
+    fetch("https://api.waifu.pics/sfw/bonk")
+    .then(response => response.json())
+    .then(content => {
+        //data, pagination, meta
+        console.log(content.url);
+        document.getElementById('waifu-gif').src = content.url;
+    });
 
     fetch('https://animechan.vercel.app/api/random')
     .then(response => response.json())
     .then(response => {
         console.log(response)
         document.getElementById('anime-quote').innerHTML = '" ' + response.quote + ' "';
-        document.getElementById('anime-character').innerHTML = "<br>" + "Said by: " + response.characte;
+        document.getElementById('anime-character').innerHTML = "<br>" + "Said by: " + response.character;
     });
+
 
 
 
